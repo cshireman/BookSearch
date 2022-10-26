f476777889ee7b59c0745a416bedb39ff5c77525
Christopher Shireman <chris@shireman.net>
2022-10-26 14:58:57 -0700

    Initial Commit

---
+++ BookSearch/BookSearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookSearchApp.swift
+//  BookSearch
+//
+//  Created by Chris Shireman on 10/26/22.
+//
+
+import SwiftUI
+
+@main
+struct BookSearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BookSearch/Credentials.swift
@@ -0,0 +1,12 @@
+//
+//  Credentials.swift
+//  BookSearch
+//
+//  Created by Chris Shireman on 10/26/22.
+//
+
+import Foundation
+
+struct Credentials {
+    let googleAPIKey = "AIzaSyCLmSN_2PS3-nhLFzZBWTC_Rb0yZL72Cew"
+}
